a7fe9366b25b8c7decb9c5fab8965a74f2b24178
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2021-07-21 12:38:54 +0100

    cart content component similar to rentals

---
+++ app/components/add-to-cart.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class RentalsComponent extends Component {
+  @service('shopping-cart') cart;
+
+  @action add(item) {
+    this.cart.add(item);
+  }
+}
--- app/components/rentals.js
+++ app/components/rentals.js
@@ -1,14 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class RentalsComponent extends Component {
   @tracked query = '';
-
-  @service('shopping-cart') cart;
-
-  @action add(item) {
-    this.cart.add(item);
-  }
 }
